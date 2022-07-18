04c22a49651b4ae3edcf51840b9c07d3ac10e6a5
Yokotes <takjchannel@gmail.com>
2022-07-18 20:24:18 +0300

    feat: add border

--- src/components/ProjectStar.tsx
+++ src/components/ProjectStar.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useMemo } from 'react'
 import {
   CircleBufferGeometry,
+  Clock,
   Color,
   Mesh,
   MeshBasicMaterial,
   Points,
+  RingBufferGeometry,
 } from 'three'
 import { useCoreContext } from '../contexts'
 
@@ -19,30 +21,50 @@ interface Props {
 
 export const ProjectStar = ({ position, galaxy }: Props) => {
   const { camera, useRenderLoop } = useCoreContext()
-  const geometry = useMemo(() => new CircleBufferGeometry(0.1, 32), [])
-  const material = useMemo(
-    () => new MeshBasicMaterial({ color: new Color('#fff') }),
+  const bgGeometry = useMemo(() => new CircleBufferGeometry(0.1, 32), [])
+  const borderGeometry = useMemo(
+    () => new RingBufferGeometry(0.1, 0.11, 32),
     []
   )
-  const circle = useMemo(
-    () => new Mesh(geometry, material),
-    [geometry, material]
+  const bgMaterial = useMemo(
+    () =>
+      new MeshBasicMaterial({
+        color: new Color('black'),
+      }),
+    []
+  )
+  const borderMaterial = useMemo(() => {
+    const material = bgMaterial.clone()
+    material.color = new Color('#fff')
+
+    return material
+  }, [bgMaterial])
+  const bgCircle = useMemo(
+    () => new Mesh(bgGeometry, bgMaterial),
+    [bgGeometry, bgMaterial]
+  )
+  const borderCircle = useMemo(
+    () => new Mesh(borderGeometry, borderMaterial),
+    [borderGeometry, borderMaterial]
   )
 
   useEffect(() => {
-    galaxy.remove(circle)
-    galaxy.add(circle)
+    galaxy.remove(bgCircle)
+    galaxy.add(bgCircle)
+
+    bgCircle.remove(borderCircle)
+    bgCircle.add(borderCircle)
 
     const { x, y, z } = position
-    circle.position.set(x, y, z)
-  }, [circle, position, galaxy])
+    bgCircle.position.set(x, y, z)
+  }, [bgCircle, position, galaxy, borderCircle])
 
   useRenderLoop(
     () => {
-      circle.lookAt(camera.position)
+      bgCircle.lookAt(camera.position)
     },
-    `${circle.id}_update`,
-    [circle, camera]
+    `${bgCircle.id}_update`,
+    [bgCircle, camera]
   )
 
   return null
