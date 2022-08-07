912553410e7065817c2910688dde930bae11dfe4
Yokotes <takjchannel@gmail.com>
2022-08-07 18:52:38 +0300

    fix: action

--- src/epics/projectStarData.ts
+++ src/epics/projectStarData.ts
@@ -2,6 +2,7 @@ import { projectStarDataFetchAction } from './../actions'
 import { isActionOf } from 'typesafe-actions'
 import { filter, from, mergeMap, of } from 'rxjs'
 import { AppEpic } from '../types'
+import { read } from 'jwt-client'
 
 export const projectStarDataFetchEpic: AppEpic = (action$) =>
   action$.pipe(
@@ -10,7 +11,9 @@ export const projectStarDataFetchEpic: AppEpic = (action$) =>
       from(
         fetch(process.env.REACT_APP_REPOS_URL as string, {
           headers: {
-            Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+            Authorization: `token ${
+              read(process.env.REACT_APP_GITHUB_TOKEN || '').claim.token
+            }`,
           },
         })
       ).pipe(
